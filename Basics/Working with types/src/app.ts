7ec394d202e08bd3669780a0b3a410abc5e3aa91
sidiik <sidiikpro@gmail.com>
2022-11-02 07:56:08 +0300

    added types

--- Basics/Working with types/src/app.ts
+++ Basics/Working with types/src/app.ts
@@ -16,6 +16,20 @@ const person: {
   role: Role.READ_ONLY,
 };
 
-if (person.role === Role.ADMIN) {
-  console.log(person);
-}
+// UNION TYPES
+
+const combine = (inputOne: string | number, inputTwo: string | number) => {
+  let result;
+  if (typeof inputOne === 'number' && typeof inputTwo === 'number') {
+    result = inputOne + inputTwo;
+  } else {
+    result = inputOne.toString() + inputTwo.toString();
+  }
+
+  return result;
+};
+
+const combinationResult = combine('sidiq', 'omar');
+const age = combine(10, 8);
+
+console.log(combinationResult, age);
