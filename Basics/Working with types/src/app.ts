2bcb496da04b54df97949a4be8b450305d1eb0e2
sidiik <sidiikpro@gmail.com>
2022-11-02 07:44:45 +0300

    enums

--- Basics/Working with types/src/app.ts
+++ Basics/Working with types/src/app.ts
@@ -1,9 +1,21 @@
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
 const person: {
   name: string;
   age: 20;
+  hobbies: string[];
+  role: Role;
 } = {
   name: 'sidiq cumar',
   age: 20,
+  hobbies: ['Sports', 'Cooking'],
+  role: Role.READ_ONLY,
 };
 
-console.log(person.age);
+if (person.role === Role.ADMIN) {
+  console.log(person);
+}
