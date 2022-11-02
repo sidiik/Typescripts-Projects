e7d4cd51b7396ebc7bb91035a668c673ed58ec29
sidiik <sidiikpro@gmail.com>
2022-11-02 07:15:20 +0300

    Done Type infrence

--- Basics/Working with types/src/app.ts
+++ Basics/Working with types/src/app.ts
@@ -2,6 +2,11 @@ function add(n1: number, n2: number) {
   return n1 + n2;
 }
 
-const result = add(3, 3);
+const result = add(3, 4);
 
 console.log(result);
+
+// TYPE INFERENCE
+let numOne: number;
+
+numOne = 2;
